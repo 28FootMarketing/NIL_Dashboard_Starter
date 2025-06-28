2467d733d6275528a811ea3340918023d19f6e10
28FootMarketing <28footmarketing@gmail.com>
2025-06-28 12:37:42 -0400

    Update nil_wizard.py

--- utils/nil_wizard.py
+++ utils/nil_wizard.py
@@ -4,7 +4,18 @@ import streamlit as st
 def run_wizard():
     st.write("🛠️ Build a mock NIL offer now:")
     brand = st.text_input("Brand Name")
-    offer_type = st.selectbox("Type of NIL Opportunity", ["Post on IG", "Wear gear", "Make a video"])
+    offer_type = st.selectbox("Type of NIL Opportunity", [
+        "Post on IG", 
+        "Wear gear", 
+        "Make a video", 
+        "Host a giveaway", 
+        "Attend an event", 
+        "Create a shoutout video", 
+        "Use a product in content", 
+        "Join a podcast", 
+        "Write a blog or caption",
+        "Other"
+    ])
     payment = st.text_input("Proposed Payment Amount ($)")
     deadline = st.date_input("Delivery Deadline")
 
