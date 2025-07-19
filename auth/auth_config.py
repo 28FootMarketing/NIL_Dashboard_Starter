b109937c95b01b6fc43b917cff6b78514cba5b6a
28FootMarketing <28footmarketing@gmail.com>
2025-07-19 14:23:10 -0400

    Update auth_config.py

--- auth/auth_config.py
+++ auth/auth_config.py
@@ -1,9 +1,3 @@
-from supabase import create_client
-import os
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 # /auth/auth_config.py
 
 # Comment out or remove this import if Supabase is not used
